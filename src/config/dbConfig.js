93f8bbde8a57728ddf64f0cce71f34709b34d4f8
Chris Answood <chrisanswood@gmail.com>
2024-02-15 00:17:53 -0300

    3

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -2,7 +2,6 @@
 var mongoose = require('mongoose');
 var dbConfig = "mongodb+srv://root:root@cluster0.ypip2k3.mongodb.net/annotations?retryWrites=true&w=majority";
 
-
 var connection = mongoose.connect(dbConfig);
 
 module.exports = connection;
